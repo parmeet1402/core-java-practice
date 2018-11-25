211571fdb6ef1d7d3c7b09fb0ec2fe89d603febc
parmeet <parmeet.asi@gmail.com>
2018-11-25 19:36:01 +0530

    added creation of thread using thread class

---
+++ multi-threading/CreateThreadUsingThreadClass.java
@@ -0,0 +1,24 @@
+class MyThread extends Thread{
+    private int threadNum;
+
+    MyThread(int n){
+        threadNum = n;
+    }
+
+    public void run(){
+        System.out.println("Thread Number:"+threadNum+" is running");
+    }
+}
+
+public class CreateThreadUsingThreadClass{
+    public static void main(String[] args) {
+        System.out.println("main thread starts");
+        MyThread t1= new MyThread(1);
+        MyThread t2= new MyThread(2);
+        MyThread t3= new MyThread(3);
+        t1.start();
+        t2.start();
+        t3.start();
+        System.out.println("main thread finished");
+    }
+}
